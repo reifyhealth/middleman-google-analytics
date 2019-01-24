c0554e32aed480b88c9ed4132138130dae209f16
khiemlam <khiem@reifyhealth.com>
2019-01-24 17:58:42 -0500

    Add cookiebot option to GA (#1)
    * Add cookiebot option to GA
    
    * Fix typo
    
    * Fix typo

--- lib/middleman-google-analytics/helpers.rb
+++ lib/middleman-google-analytics/helpers.rb
@@ -18,7 +18,11 @@ module Middleman
 
         if options.output.to_sym == :html
           content = indent(content) unless options.minify
-          content_tag(:script, content)
+          if options.cookiebot
+            content_tag(:script, content, :class => 'text/plain', 'data-cookieconsent' => 'statistics')
+          else
+            content_tag(:script, content)
+          end  
         else
           content
         end
